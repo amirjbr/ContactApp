0df0e7031c97ff75af288d7af2319970aa881303
amirjbr <153921915+amirjbr@users.noreply.github.com>
2023-12-24 00:21:41 +0330

    Add project files.

---
+++ ContactApp/NewContactWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using ContactApp.Classes;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ContactApp
+{
+    /// <summary>
+    /// Interaction logic for NewContactWindow.xaml
+    /// </summary>
+    public partial class NewContactWindow : Window
+    {
+        public NewContactWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void saveButton_Click(object sender, RoutedEventArgs e)
+        {
+            //Save Contact 
+            Contact contact = new Contact()
+            {
+                Name = NameTextBox.Text,
+                Email = EmailTextBox.Text,
+                Phone = PhoneTextBox.Text
+
+            };
+            string databaseName = "Contact.db";
+            string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            string databasePath = System.IO.Path.Combine(folderPath,databaseName);
+
+            SQLiteConnection connection = new SQLiteConnection(databasePath);
+            connection.CreateTable<Contact>();
+            connection.Insert(contact);
+
+
+
+
+            Close();
+        }
+
+        
+    }
+}
